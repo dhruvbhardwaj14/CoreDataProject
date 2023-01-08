ce36b9968d2d05c466cba0f0089288d9eee67f09
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2023-01-08 21:40:23 +0530

    Core Data Fundamental Project

--- CoreDataProject/CoreDataProjectApp.swift
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct CoreDataProjectApp: App {
+    @StateObject private var dataController=Datacontroller()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
